a291a196d93c469a184825c11d7689ea9c950e6b
Julien Capellari <julien.capellari@gmail.com>
2025-12-01 23:06:19 +0100

    feat: resolve day 01 (part 02)

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -12,22 +12,41 @@ macro_rules! read_lines {
 fn main() {
     let mut state = 50;
     let mut part1 = 0;
+    let mut part2 = 0;
 
     for line in read_lines!("day-01/input.txt") {
-        let cnt = &line[1..].parse().unwrap();
+        let cnt: i32 = (&line[1..]).parse().unwrap();
 
         if &line[0..1] == "R" {
-            state = (state + cnt) % 100;
-        } else {
-            state = (state - cnt + 100) % 100;
-        }
+            for _ in 0..cnt {
+                state += 1;
 
-        // println!("{line} => {state}");
+                if state == 100 {
+                    state = 0;
+                    part2 += 1;
+                }
+            }
+        } else {
+            for _ in 0..cnt {
+                state -= 1;
+
+                if state == 0 {
+                    part2 += 1;
+                }
+
+                if state == -1 {
+                    state = 99;
+                }
+            }
+        }
 
         if state == 0 {
             part1 += 1;
         }
+
+        println!("{line:<4} => {state:<3}  {part1:<4}  {part2:<4}");
     }
 
     println!("part 01: {part1}");
+    println!("part 02: {part2}");
 }
